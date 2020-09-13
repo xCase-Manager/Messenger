a575bcff31fafcacaff3306a2826b60648aa53d4
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-13 14:56:02 +0200

    disable json-ignore

--- src/main/java/org/xcasemanager/messenger/web/rest/resource/ExecutionMessageDto.java
+++ src/main/java/org/xcasemanager/messenger/web/rest/resource/ExecutionMessageDto.java
@@ -2,12 +2,19 @@ package org.xcasemanager.messenger.web.rest.resource;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = false)
 public class ExecutionMessageDto {
 
+    @NotNull
+    @Min(5)
     private String executionId;
 
+    @NotNull
+    @Size(min=2, max=30)
     private String result;
 }
