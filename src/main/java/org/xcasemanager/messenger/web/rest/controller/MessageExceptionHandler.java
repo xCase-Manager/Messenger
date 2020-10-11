71f1d89288646678152459ce83f3ffd2d691fc56
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-11 13:45:40 +0200

    removed obsolete classes

--- src/main/java/org/xcasemanager/messenger/web/rest/controller/MessageExceptionHandler.java
+++ src/main/java/org/xcasemanager/messenger/web/rest/controller/MessageExceptionHandler.java
@@ -10,13 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.xcasemanager.messenger.web.rest.controller.MessageException;
-import org.xcasemanager.messenger.web.rest.controller.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.xcasemanager.messenger.web.rest.controller.MessageException;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.FieldError;
 
@@ -25,17 +21,7 @@ import org.springframework.validation.FieldError;
 @ControllerAdvice
 public class MessageExceptionHandler extends ResponseEntityExceptionHandler 
 {
-    private String INCORRECT_REQUEST = "INCORRECT_REQUEST";
-    private String BAD_REQUEST = "BAD_REQUEST";
     private static final Logger log = LoggerFactory.getLogger(MessageExceptionHandler.class);
-
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ErrorResponse> handleInvalidMessageException
-                        (Exception ex, WebRequest request) {
-        log.info(" +++++++++---------->> Message error excetion ");
-        ErrorResponse error = new ErrorResponse("Validation Failed", null);
-        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
-    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
